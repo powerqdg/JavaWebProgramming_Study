daaa34661b2c31dcf623620701200b0327acb8dc
power <power@DESKTOP-J5GMKLH>
2022-04-01 21:41:04 +0900

    step07-1

---
+++ web04/src/spms/servlets/MemberUpdateServlet.java
@@ -0,0 +1,111 @@
+package spms.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/member/update")
+@SuppressWarnings("serial")
+public class MemberUpdateServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		
+		try {
+			//1. 사용할 JDBC 드라이버를 등록하라.
+			DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+			
+			//2. 드라이버를 사용하어 Oracle 서버와 연결하라.
+			conn = DriverManager.getConnection(
+					"jdbc:oracle:thin:@localhost:1521:xe", //JDBC URL
+					"study",	// DBMS 사용자 아이디
+					"study");	// DBMS 사용자 암호
+			
+			//3. 커넥션 객체로부터 SQL을 던질 객체를 준비하라.
+			stmt = conn.createStatement();
+			
+			//4. SQL을 던지는 객체를  사용하여 서버에 질의하라!
+			rs = stmt.executeQuery(
+					"SELECT MNO,MNAME,EMAIL,CRE_DATE" + 
+					" FROM MEMBERS" +
+					" WHERE MNO=" + request.getParameter("no"));
+			rs.next();
+			
+			response.setContentType("text/html; charset=UTF-8");
+			PrintWriter out = response.getWriter();
+			out.println("<html><head><title>회원정보</title></head>");
+			out.println("<body><h1>회원정보</h1>");
+			out.println("<form action='update' method='post'>");
+			out.println("번호: <input type='text' name='no' value='" + request.getParameter("no") + "' readonly><br>");
+			out.println("이름: <input type='text' name='name'" + " value='" + rs.getString("MNAME")  + "'><br>");
+			out.println("이메일: <input type='text' name='email'" + " value='" + rs.getString("EMAIL")  + "'><br>");
+			out.println("가입일: " + rs.getDate("CRE_DATE") + "<br>");
+			out.println("<input type='submit' value='저장'>");
+			out.println("<input type='button' value='취소'" + " onclick='location.href=\"list\"'>");
+			out.println("</form>");
+			out.println("</body></html>");
+			
+			try {if (rs != null) rs.close();} catch(Exception e) {}
+			try {if (stmt != null) stmt.close();} catch(Exception e) {}
+			try {if (conn != null) conn.close();} catch(Exception e) {}
+		} catch (Exception e) {
+			throw new ServletException(e);
+			
+		} finally {
+			try {if (rs != null) rs.close();} catch(Exception e) {}
+			try {if (stmt != null) stmt.close();} catch(Exception e) {}
+			try {if (conn != null) conn.close();} catch(Exception e) {}
+		}
+		
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8"); // 한글 깨짐 방지
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		
+		try {
+			//1. 사용할 JDBC 드라이버를 등록하라.
+			DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+			
+			//2. 드라이버를 사용하어 Oracle 서버와 연결하라.
+			conn = DriverManager.getConnection(
+					"jdbc:oracle:thin:@localhost:1521:xe", //JDBC URL
+					"study",	// DBMS 사용자 아이디
+					"study");	// DBMS 사용자 암호
+			
+			//3. 커넥션 객체로부터 SQL을 던질 객체를 준비하라.
+			stmt = conn.prepareStatement(
+					"UPDATE MEMBERS SET EMAIL=?,MNAME=?,MOD_DATE=SYSDATE"
+					+ " WHERE MNO=?");
+			stmt.setString(1, request.getParameter("email"));
+			stmt.setString(2, request.getParameter("name"));
+			stmt.setInt(3, Integer.parseInt(request.getParameter("no")));
+			stmt.executeUpdate();
+			
+			response.sendRedirect("list");
+			
+			try {if (stmt != null) stmt.close();} catch(Exception e) {}
+			try {if (conn != null) conn.close();} catch(Exception e) {}
+		} catch (Exception e) {
+			throw new ServletException(e);
+			
+		} finally {
+			try {if (stmt != null) stmt.close();} catch(Exception e) {}
+			try {if (conn != null) conn.close();} catch(Exception e) {}
+		}
+	}
+}
