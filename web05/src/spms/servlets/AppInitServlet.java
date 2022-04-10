048eb9928e2299d55f6b5361dd12b117ed5d8d9b
power <power@DESKTOP-J5GMKLH>
2022-04-10 20:22:50 +0900

    추가 서블릿

---
+++ web05/src/spms/servlets/AppInitServlet.java
@@ -0,0 +1,41 @@
+package spms.servlets;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+
+@SuppressWarnings("serial")
+public class AppInitServlet extends HttpServlet{
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("AppInitServlet init()");
+		super.init(config);
+		try {
+			ServletContext sc = this.getServletContext();
+			Class.forName(sc.getInitParameter("driver"));
+			Connection conn = DriverManager.getConnection(
+						sc.getInitParameter("url"),
+						sc.getInitParameter("username"),
+						sc.getInitParameter("password"));
+			sc.setAttribute("conn", conn);
+		} catch(Throwable e) {
+			throw new ServletException(e);
+		}
+	}
+		
+	@Override
+	public void destroy() {
+		System.out.println("AppInitServlet destroy()");
+		super.destroy();
+		Connection conn = (Connection)this.getServletContext().getAttribute("conn"); 
+		try {
+			if (conn != null && conn.isClosed() == false) {
+				conn.close();
+			}
+		} catch (Exception e) {}
+	}
+}
