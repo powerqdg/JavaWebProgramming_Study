82ed77ed3b106a7fb698ad4755ba5ac905f728fd
power <power@DESKTOP-J5GMKLH>
2022-04-10 18:03:53 +0900

    step09-2

---
+++ web04/src/spms/servlets/MemberDeleteServlet.java
@@ -0,0 +1,45 @@
+package spms.servlets;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+@WebServlet("/member/delete")
+public class MemberDeleteServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		
+		try {
+			ServletContext ctx = this.getServletContext();
+			Class.forName(ctx.getInitParameter("driver"));
+			
+			conn = DriverManager.getConnection(
+					ctx.getInitParameter("url"), //JDBC URL
+					ctx.getInitParameter("username"),	// DBMS 사용자 아이디
+					ctx.getInitParameter("password"));	// DBMS 사용자 암호
+			stmt = conn.prepareStatement("DELETE FROM MEMBERS WHERE MNO=?");
+			stmt.setInt(1, Integer.parseInt(request.getParameter("no")));
+			stmt.executeUpdate();
+			
+			response.sendRedirect("list");
+			try {if (stmt != null) stmt.close();} catch(Exception e) {}
+			try {if (conn != null) conn.close();} catch(Exception e) {}
+		} catch (Exception e) {
+			throw new ServletException(e);
+		} finally {
+			try {if (stmt != null) stmt.close();} catch(Exception e) {}
+			try {if (conn != null) conn.close();} catch(Exception e) {}
+		}
+	}
+}
